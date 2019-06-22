f346a22117e84ac832e27d94cfb9bf8993c744ae
lgybetter <437675103@qq.com>
2019-06-22 16:56:00 +0800

    :art: animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('test')
+import './css/index.css';
+
+import $ from 'jquery';
+import FullPage from 'fullpage.js';
+
+new FullPage('#h5', {
+  onLeave (origin, { index }, direction) {
+    $('.page').find('.component').eq(index).fadeOut();
+  },
+  afterLoad (origin, { index }, direction) {
+    $('.page').find('.component').eq(index).fadeIn();
+  }
+})
